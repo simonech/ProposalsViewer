4d0e28c7fee255ee8b49e6654249b716359d2730
Simone Chiaretta <simone.chiaretta@consilium.europa.eu>
2015-05-22 16:54:04 +0200

    All files

---
+++ js/controllers.js
@@ -0,0 +1,34 @@
+var phonecatApp = angular.module('proposalsApp', []);
+
+phonecatApp.controller('ProposalsCtrl', function ($scope, $filter, $http) {
+
+    $scope.proposals = [];
+    $http.get('data/proposals.json').success(function(data) {
+      $scope.proposals = data.proposals;
+    });
+    
+    var authors = [];
+    $scope.proposals.forEach(function(obj){
+        var matchingAuthor = $filter('filter')(authors, function(value, index){console.log(value[0]); return value[0].lastname==obj.author.lastname}, true);
+        if(matchingAuthor.length==0)
+            authors.push([obj.author,1]);
+        else
+            matchingAuthor[0][1]++;
+    });
+
+    $scope.authors = authors;
+
+    var tags = [];
+    $scope.proposals.forEach(function(obj){
+        obj.tags.forEach(function (innerObj){
+            var matchingTag = $filter('filter')(tags, function(value, index){return value[0]==innerObj}, true);
+            if(matchingTag.length==0)
+                tags.push([innerObj,1]);
+            else
+                matchingTag[0][1]++;
+        })
+    });
+
+    $scope.tags = tags
+
+});
